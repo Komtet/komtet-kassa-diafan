e9807c5908b3a40e9cde57163acd087282c5586a
Elatonsev Mihail <elatonsev.mihail@gmail.com>
2019-02-08 16:31:56 +0300

    Сборка проекта через докер

--- src/modules/shop/inc/shop.inc.order.custom.php
+++ src/modules/shop/inc/shop.inc.order.custom.php
@@ -1,7 +1,7 @@
-class Shop_inc_order extends Diafan
-{
-  after public function set_status ($order, $status)
-  {
+<?php
+
+class Shop_inc_order extends Diafan {
+  after public function set_status ($order, $status) {
     $this->diafan->_komtetkassa->print_check($order['id']);
   }
 }
